dacd78e2dc56d474bb6bc62f5557fde64d33644d
Benarji Anand <benarji385@gmail.com>
2023-07-06 19:36:43 +0800

    msm8956-common: Add HIDL device implementations
    Add the list of HAL interfaces to be implemented.
    
    Signed-off-by: Benarji Anand <benarji385@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -162,6 +162,10 @@ USE_DEVICE_SPECIFIC_GPS := true
 USE_DEVICE_SPECIFIC_LOC_API := true
 TARGET_NO_RPC := true
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
